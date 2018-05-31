6bddffa4a3ae990951a29758492e935357751665
Xinyu Liu <meteor.lxy@foxmail.com>
2018-05-31 10:02:16 +0800

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  chainWebpack: (config, isServer) => {
+    // !exclamatory mark in comments or codes will both cause errors
+
+    let test = true
+    if (!test) {
+      // ...
+    }
+  }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+Repro steps
+
+```
+yarn
+yarn dev
+```
